e15d70c9958bb225686272053629cb9f72e72c09
Keimel R <keimelrodriguez12@gmail.com>
2022-09-18 19:17:02 -0430

    Incorporacion de funciones borradas

--- handlers/facturas.h
+++ handlers/facturas.h
@@ -1,3 +1,25 @@
+int create_new_invoices(char *name, char *zone, char *dni, char *id_venta, char *price);
+int delete_invoice(char *ID);
+
+
+int modify_name_invoice(int row, char *str);
+int modify_zone_invoice(int row, char *str);
+int modify_dni_invoice(int row, char *str);
+int modify_date_invoice(int row, char *str);
+int modify_id_venta_invoice(int row, char *str);
+int modify_price_invoice(int row, char *str);
+
+
+int get_name_invoice(int row, char *str);
+int get_zone_invoice(int row, char *str);
+int get_dni_invoice(int row, char *str);
+int get_date_invoice(int row, char *str);
+int get_ID_venta_invoice(int row, char *str);
+int get_price_invoice(int row, char *str);
+char get_naturaleza(int row);
+
+/////////////////////////////////////////7
+
 void empresa();	// Muestra los datos de la empresa	
 void factura_user(FILE *archivo, char code[], char name[], char city[], char rif[], char total[]);	//	Muestra en pantalla todos los elementos de la factura (falta productos)
 
